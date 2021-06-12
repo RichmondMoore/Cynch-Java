54794bd5a495720aa184c2b8fe6a5a33fd6edba1
Richmond Moore <richmondwmoore01@gmail.com>
2021-06-12 14:00:21 -0400

    Comment to explain the expression grammar of Cynch

--- src/main/java/Expr.java
+++ src/main/java/Expr.java
@@ -80,3 +80,19 @@ abstract class Expr {
 
   abstract <R> R accept(Visitor<R> visitor);
 }
+
+/*
+    Cynch Grammer:
+
+    expression     → literal
+                   | unary
+                   | binary
+                   | grouping ;
+
+    literal        → NUMBER | STRING | "true" | "false" | "nil" ;
+    grouping       → "(" expression ")" ;
+    unary          → ( "-" | "!" ) expression ;
+    binary         → expression operator expression ;
+    operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
+                   | "+"  | "-"  | "*" | "/" ;
+*/
