a9258a1e6962e321a44557ad3ca94bfd5c08edf6
Richmond Moore <richmondwmoore01@gmail.com>
2021-06-19 21:09:03 -0400

    Variable assignment support

--- src/main/java/Expr.java
+++ src/main/java/Expr.java
@@ -8,12 +8,27 @@ import java.util.List;
 // Grouping.left)
 abstract class Expr {
     interface Visitor<R> {
+        R visitAssignExpr(Assign expr);
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
     }
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+  }
+
+    final Token name;
+    final Expr value;
+  }
 
   // Operators that need two operands (ex. +, -, *, /, and logic operators)
   static class Binary extends Expr {
