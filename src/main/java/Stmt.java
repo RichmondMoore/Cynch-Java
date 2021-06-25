04789c570b8af40df0f52da6df044353ecba1171
Richmond Moore <richmondwmoore01@gmail.com>
2021-06-24 20:18:38 -0400

    Support for function and return statements

--- src/main/java/Stmt.java
+++ src/main/java/Stmt.java
@@ -8,7 +8,9 @@ abstract class Stmt {
         R visitBreakStmt(Break stmt);
         R visitExpressionStmt(Expression stmt);
         R visitIfStmt(If stmt);
+        R visitFunctionStmt(Function stmt);
         R visitPrintStmt(Print stmt);
+        R visitReturnStmt(Return stmt);
         R visitVarStmt(Var stmt);
         R visitWhileStmt(While stmt);
     }
@@ -62,6 +64,22 @@ abstract class Stmt {
     final Stmt thenBranch;
     final Stmt elseBranch;
   }
+  static class Function extends Stmt {
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+  }
+
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
+  }
   static class Print extends Stmt {
     Print(Expr expression) {
       this.expression = expression;
@@ -73,6 +91,20 @@ abstract class Stmt {
   }
 
     final Expr expression;
+  }
+  static class Return extends Stmt {
+    Return(Token keyword, Expr value) {
+      this.keyword = keyword;
+      this.value = value;
+    }
+
+  @Override
+  <R> R accept(Visitor<R> visitor) {
+      return visitor.visitReturnStmt(this);
+  }
+
+    final Token keyword;
+    final Expr value;
   }
   static class Var extends Stmt {
     Var(Token name, Expr initializer) {
